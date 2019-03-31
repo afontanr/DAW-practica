a6261cd5ac902ebde53c00e7a935b712776efa47
afontanr <afontanr@gmail.com>
2019-03-31 18:42:54 +0200

    Realizando ultimos cambios en navbar y tablas

--- frontend/scripts/scriptMostrarPelicula.js
+++ frontend/scripts/scriptMostrarPelicula.js
@@ -1,7 +1,15 @@
 $(document).ready(function(){
 
 
-  $('#myModal').on('shown.bs.modal', function () {
+  $('#myModal1').on('shown.bs.modal', function () {
+    $('#myInput').focus()
+  });
+
+  $('#myModal2').on('shown.bs.modal', function () {
+    $('#myInput').focus()
+  });
+
+  $('#myModal3').on('shown.bs.modal', function () {
     $('#myInput').focus()
   });
 
